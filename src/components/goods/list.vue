d0be87e59db16a3a974b679b7e27f212556f6913
ZAHNGFEI <ainiyou.xls@gmail.com>
2019-12-26 00:03:34 +0800

    根据id查询商品信息

--- src/components/goods/list.vue
+++ src/components/goods/list.vue
@@ -16,28 +16,116 @@
             </el-input>
          </el-col>
          <el-col :span='3'>
-             <el-button type='primary'>添加商品</el-button>
+             <el-button type='primary' @click="showAddGoodsDialog">添加商品</el-button>
          </el-col>
        </el-row>
        <!-- 商品列表 -->
-       <el-table :data='goodList'>
+       <el-table :data='goodList' max-height="600">
            <el-table-column type='index'></el-table-column>
-           <el-table-column label='商品名称' prop='goods_name' width='450px'></el-table-column>
-           <el-table-column label='商品价格(元)' prop='goods_price' width='120px'></el-table-column>
-           <el-table-column label='商品重量' prop='goods_weight' width='120px'></el-table-column>
-           <el-table-column label='创建时间' prop='add_time' wdith='190px'>
+           <el-table-column label='商品名称' prop='goods_name' width='750' ></el-table-column>
+           <el-table-column label='商品价格(元)' prop='goods_price' ></el-table-column>
+           <el-table-column label='商品重量' prop='goods_weight' ></el-table-column>
+           <el-table-column label='创建时间' prop='add_time'  >
              <template slot-scope="scope">
                {{scope.row.add_time | dateFormat}}
              </template>
            </el-table-column>
-           <el-table-column label='操作'>
+           <el-table-column label='操作' >
              <template slot-scope="scope">
-               <el-button type="primary" icon="el-icon-edit" size='mini'></el-button>
+               <el-button type="primary" icon="el-icon-edit" size='mini' @click="editGoods(scope.row.goods_id)"></el-button>
                <el-button type="danger" icon="el-icon-delete" size='mini'></el-button>
-             </template>        
+             </template>  
            </el-table-column>
        </el-table>
+          <!-- 页码       -->
+             <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="queryGoodsList.pagenum"
+             :page-sizes="[5, 10, 30, 40]" :page-size="queryGoodsList.pagesize" layout="total, sizes, prev, pager, next, jumper" :total="total">
+           </el-pagination>
     </el-card>
+
+    <!-- 添加商品Dialog -->
+    <el-dialog title="添加商品" :visible.sync="addGoodsdialogVisible" width="50%">
+      <!-- 添加商品的表单 -->
+        <el-form :model="addGoodsList" :rules="addrules" ref="addruleFormRef" label-width="100px" class="demo-ruleForm">
+          <el-form-item label="商品名称" prop="goods_name">
+            <el-input v-model="addGoodsList.goods_name"></el-input>
+          </el-form-item>
+
+           <el-form-item label="goods_cat" prop="goods_cat">
+            <el-input v-model="addGoodsList.goods_name"></el-input>
+          </el-form-item>
+
+           <el-form-item label="价格" prop="goods_price">
+            <el-input v-model="addGoodsList.goods_price"></el-input>
+          </el-form-item>
+
+           <el-form-item label="数量" prop="goods_number">
+            <el-input v-model="addGoodsList.goods_number"></el-input>
+          </el-form-item>
+
+           <el-form-item label="重量" prop="goods_weight">
+            <el-input v-model="addGoodsList.goods_weight"></el-input>
+          </el-form-item>
+
+          <el-form-item label="简单介绍" prop="goods_introduce">
+            <el-input v-model="addGoodsList.goods_introduce"></el-input>
+          </el-form-item>
+
+           <!-- <el-form-item label="简单介绍" prop="goods_introduce">
+            <el-input v-model="addGoodsList.goods_introduce"></el-input>
+          </el-form-item>
+
+           <el-form-item label="简单介绍" prop="goods_introduce">
+            <el-input v-model="addGoodsList.goods_introduce"></el-input>
+          </el-form-item> -->
+        </el-form>
+      <span slot="footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+      <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+  </span>
+</el-dialog>
+
+   <!-- 查询商品Dialog -->
+    <el-dialog title="修改商品" :visible.sync="editGoodsdialogVisible" width="50%">
+      <!-- 查询商品的表单 -->
+        <el-form :model="editGoodsList" :rules="editrules" ref="editruleFormRef" label-width="100px" class="demo-ruleForm">
+          <el-form-item label="商品名称" prop="goods_name">
+            <el-input v-model="editGoodsList.goods_name"></el-input>
+          </el-form-item>
+
+           <el-form-item label="goods_cat" prop="goods_cat">
+            <el-input v-model="editGoodsList.goods_cat"></el-input>
+          </el-form-item>
+
+           <el-form-item label="价格" prop="goods_price">
+            <el-input v-model="editGoodsList.goods_price"></el-input>
+          </el-form-item>
+
+           <el-form-item label="数量" prop="goods_number">
+            <el-input v-model="editGoodsList.goods_number"></el-input>
+          </el-form-item>
+
+           <el-form-item label="重量" prop="goods_weight">
+            <el-input v-model="editGoodsList.goods_weight"></el-input>
+          </el-form-item>
+
+          <el-form-item label="简单介绍" prop="goods_introduce">
+            <el-input v-model="editGoodsList.goods_introduce"></el-input>
+          </el-form-item>
+
+           <el-form-item label="简单介绍" prop="goods_introduce">
+            <el-input v-model="editGoodsList.goods_introduce"></el-input>
+          </el-form-item>
+
+           <el-form-item label="简单介绍" prop="goods_introduce">
+            <el-input v-model="editGoodsList.goods_introduce"></el-input>
+          </el-form-item>
+        </el-form>
+      <span slot="footer">
+        <el-button @click="editGoodsdialogVisible = false">取 消</el-button>
+      <el-button type="primary" @click="editGoodsdialogVisible = false">确 定</el-button>
+  </span>
+</el-dialog>
   </div>  
 </template>
 
@@ -46,8 +134,44 @@ export default {
     data() {
         return {
           //商品列表
+          queryGoodsList:{
+            query:'',
+            pagenum:1,
+            pagesize:10
+          },
           goodList:[],
-          total:''
+          total:0,
+          addGoodsdialogVisible:false,
+          editGoodsdialogVisible:false,
+          addGoodsList:{
+            goods_name:'',
+            goods_cat:'',
+            goods_price :'',
+            goods_number:'',
+            goods_weight:'',
+            goods_introduce:'',
+            pics:[],
+            attrs:[]
+          },
+          editGoodsList:{},
+          addrules:{
+            goods_name:[
+                { required: true, message: '请输入活动名称', trigger: 'blur' }
+            ],
+             goods_cat:[
+               { required: true, message: '请输入活动名称', trigger: 'blur' }
+            ],
+              goods_price :[
+               { required: true, message: '请输入活动名称', trigger: 'blur' }
+            ],
+               goods_number:[
+               { required: true, message: '请输入活动名称', trigger: 'blur' }
+            ],
+                goods_weight:[
+               { required: true, message: '请输入活动名称', trigger: 'blur' }
+            ]
+
+          }
         }
     },
     created() {
@@ -57,11 +181,7 @@ export default {
         //获取商品列表
         getGoodList(){
             var that = this
-            this.$http.get('goods',{params:{
-                query:'',
-                pagenum:1,
-                pagesize:13}
-            }).then(function(result){
+            this.$http.get('goods',{params:this.queryGoodsList}).then(function(result){
                 const res = result.data
                 if(res.meta.status !== 200){
                     console.log(res);
@@ -72,8 +192,37 @@ export default {
                 console.log(res.data.goods);
                 that.$message.success('获取商品列表成功')  
             })
-
+        },
+        showAddGoodsDialog(){
+          this.addGoodsdialogVisible = true
+        },
+        handleSizeChange(newsize){
+           this.queryGoodsList.pagesize = newsize
+           this.getGoodList()
+           console.log('newsixe'+newsize);
+        },
+        handleCurrentChange(newPage){
+          this.queryGoodsList.pagenum = newPage
+          this.getGoodList()
+          console.log('page'+newPage);
+        },
+        editGoods(goodId){
+          this.editGoodsdialogVisible = true
+          var that = this
+          this.$http.get(`goods/${goodId}`).then(function(result){
+            var res = result.data
+            if(res.meta.status !== 200) {
+              console.log(res);
+              return that.$message.error('获取该商品信息失败')
+            }
+            that.editGoodsList = res.data
+            console.log(res);
+            that.$message.success('获取该商品的信息成功')
+           
+          })
+        
         }
+
     },
 }
 </script>
