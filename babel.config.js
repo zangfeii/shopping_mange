cd91fe5f0ea1d7ade48390df09881380c3d0b2f0
ZAHNGFEI <ainiyou.xls@gmail.com>
2019-12-29 23:58:40 +0800

    最最最终版

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,8 @@
-const prodPlugins = []
-if (process.env.NODE_ENV === 'production') {
-    //产品处于发布阶段 不显示console.log
-    prodPlugins.push("transform-remove-console")
-}
-
 module.exports = {
     presets: [
         '@vue/cli-plugin-babel/preset'
     ],
     plugins: [
         "transform-remove-console"
-    ],
-    //发布产品阶段
-    ...prodPlugins
+    ]
 }
